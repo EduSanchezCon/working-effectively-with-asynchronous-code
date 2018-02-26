c9949f322acb3f146685fc5eeb9e7dc821656070
Eduardo Sanchez <esanchez@autentia.com>
2018-02-26 23:54:37 +0100

    Added Test to show blocking compositions

---
+++ src/test/java/com/edusancon/wewac/CompositionTest.java
@@ -0,0 +1,30 @@
+package com.edusancon.wewac;
+
+import com.edusancon.wewac.util.ThreadHelper;
+import org.junit.jupiter.api.Test;
+
+import java.util.concurrent.CompletableFuture;
+
+public class CompositionTest {
+
+    @Test
+    public void CompositionQuiz(){
+
+        final CompletableFuture<Void> future1 = CompletableFuture.runAsync(() -> {
+            ThreadHelper.sleep(1000);
+            System.out.println( "primer futuro" );
+        });
+
+        future1.thenRun( () -> {
+            ThreadHelper.sleep(1000);
+            System.out.println( "segundo futuro" );
+        });
+
+        future1.thenRun( () -> {
+            ThreadHelper.sleep(1000);
+            System.out.println( "tercer futuro" );
+        });
+
+        ThreadHelper.sleep(3500);
+    }
+}
